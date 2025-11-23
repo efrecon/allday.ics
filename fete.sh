0d55d93a4a2ec663a933e63dee894cca898bbd25
Emmanuel Frécon <efrecon@gmail.com>
2025-11-23 22:51:41 +0100

    🧑‍💻 Comment and Parametrize
    + Comment properly all variables.
    + Parameterize the IP address guessing service.
    + Require jq command.
    + Remove language parameter, always use French.
    + Remove unused code.

--- fete.sh
+++ fete.sh
@@ -5,27 +5,25 @@ set -eu
 # shellcheck disable=SC3040 # now part of POSIX, but not everywhere yet!
 if set -o | grep -q 'pipefail'; then set -o pipefail; fi
 
-# Root directory where this script is located
-: "${FETE_ROOTDIR:="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"}"
-
-# Root directory for downloaded data
-: "${FETE_DATA_ROOT:="${FETE_ROOTDIR}/data"}"
-
 # Number of days around today to include in the calendar. Default is 7 (one week
 # before and one week after today). When empty, the entire year is included.
 : "${FETE_DAYS:="7"}"
 
-# You (external) IP address.
+# You (external) IP address. Will be guessed if not provided.
 : "${FETE_IP:=""}"
 
-# Key for your IP address, obtained from https://fetedujour.fr/api
+# Key for your IP address, obtained from https://fetedujour.fr/api. Will be
+# acquired if not provided.
 : "${FETE_KEY:=""}"
 
+# Base URL for FeteDuJour API (no version)
 : "${FETE_DUJOUR_API:="https://fetedujour.fr/api"}"
 
-# Language for entries in the calendar. This should match the language used in
-# the data files, so used when running dump.sh. When empty, no language will be
-# set, this is the default.
+# How to guess the IP address, can be "icanhazip" or "ifconfigme"
+: "${FETE_IP_GUESS:="https://icanhazip.com"}"
+
+# Language for entries in the calendar. Default is "fr-FR" (French), there is
+# little point in changing this.
 : "${FETE_LANGUAGE:="fr-FR"}"
 
 # Verbosity level, can be increased with -v option
@@ -34,7 +32,7 @@ if set -o | grep -q 'pipefail'; then set -o pipefail; fi
 usage() {
   # This uses the comments behind the options to show the help. Not extremely
   # correct, but effective and simple.
-  echo "$0 saves relevant TMDB entity to disk" && \
+  echo "$0 generates ICS for surnames" && \
     grep "[[:space:]].)[[:space:]][[:space:]]*#" "$0" |
     sed 's/#//' |
     sed -E 's/([a-zA-Z-])\)/-\1/'
@@ -44,12 +42,14 @@ usage() {
 }
 
 # Parse named arguments using getopts
-while getopts ":d:l:vh-" opt; do
+while getopts ":d:k:i:vh-" opt; do
   case "$opt" in
     d) # Number of days around today to include in the calendar. Empty means entire year.
       FETE_DAYS=$OPTARG;;
-    l) # Language for entries in the calendar
-      FETE_LANGUAGE=$OPTARG;;
+    k) # API key for your IP address. Will be acquired if not provided.
+      FETE_KEY=$OPTARG;;
+    i) # Your (external) IP address. Will be guessed if not provided.
+      FETE_IP=$OPTARG;;
     v) # Increase verbosity each time repeated
       FETE_VERBOSE=$(( FETE_VERBOSE + 1 ));;
     h) # Show this help
@@ -121,25 +121,8 @@ date_interval() {
 }
 
 
-# Pick the most popular person born on a given month-day.
-# $1: month-day in MM-DD format
-most_popular_person() {
-  birthday=$1
-  info "Picking most popular person born on %s" "$1"
-  "$FETE_SELECT" \
-    -k 'popularity' \
-    -w 'birthday' \
-    -q "$1" \
-    -r "${1}\$" \
-      -- \
-        ${FETE_DATA_ROOT%%/}/person |
-      sort -k 1 -g -r |
-      head -n 1 |
-      cut -f2
-}
-
-
 ics_line_endings() { tr -d '\r' | sed 's/$/\r/'; }
+
 
 ics_localized() {
   if [ -n "$FETE_LANGUAGE" ]; then
@@ -193,7 +176,7 @@ ics_entry() {
   cat <<EOF
 BEGIN:VEVENT
 CLASS:PUBLIC
-UID:$(date -u -d "$today" +'%Y%m%d')-${2}@fetedujour.fr
+UID:$(date -u -d "$today" +'%Y%m%d')-${month}-${day}@fetedujour.fr
 DTSTAMP:$(date -u +'%Y%m%dT%H%M%SZ')
 DTSTART;VALUE=DATE:$(date -u -d "$today" +'%Y%m%d')
 RRULE:FREQ=YEARLY;INTERVAL=1;BYMONTH=$month;BYMONTHDAY=$day
@@ -239,11 +222,12 @@ silent() { "$@" >/dev/null 2>&1 </dev/null; }
 
 # Verify required commands are available
 silent command -v fold || error "fold command not found"
+silent command -v jq || error "jq command not found"
 
 # Acquire API key if not provided, this parses the HTML page so may break if the
 # page layout changes.
 if [ -z "$FETE_KEY" ]; then
-  [ -z "$FETE_IP" ] && FETE_IP=$(run_curl 'https://icanhazip.com' | tr -d '\n')
+  [ -z "$FETE_IP" ] && FETE_IP=$(run_curl "$FETE_IP_GUESS" | tr -d '\n')
   [ -z "$FETE_IP" ] && error "No IP address provided or found"
 
   FETE_KEY=$( run_curl -d "ip=${FETE_IP}" "${FETE_DUJOUR_API%%/}/#apiKey" |
